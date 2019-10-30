dba95a7ffa923106f61a6c7bce0a6a291706d66a
inqoyasha <shamsutdinovxd@yandex.ru>
2019-10-30 18:14:55 +0300

    rename package

--- src/org/azamat/homeworks/hw1/factorial/RecursionFactorial.java
+++
@@ -1,9 +0,0 @@
-package org.azamat.homeworks.hw1.factorial;
-
-public class RecursionFactorial {
-    long s=0;
-    public int fact(int n) {
-        if (n==0 || n==1) return 1;
-        return fact(n-1) * n;
-    }
-}
--- src/org/azamat/homeworks/hw1/printRect/Main.java
+++
@@ -1,8 +0,0 @@
-package org.azamat.homeworks.hw1.printRect;
-
-public class Main {
-    public static void main(String[] args) {
-        Printer p = new Printer();
-        p.printEightRect(7);
-    }
-}
